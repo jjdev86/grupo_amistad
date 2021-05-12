ab6300e44ce044238d5e815ef2a8025f41013177
Jose Valadez <vinmajj@gmail.com>
2021-05-11 17:11:10 -0700

    cleared mysql code as it was not needed in app.js

--- server/app.js
+++ server/app.js
@@ -1,43 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
-const mysql = require("mysql");
-const util = require("util"); // enable native async
+
 const path = require('path');
 
 const userRoutes = require("./routes/user");
 const reportRoutes = require("./routes/report");
 const grouptRoutes = require('./routes/group');
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gda"
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("Database connection was closed.");
-    }
-    if (err.code === "ER_CON_COUNT_ERROR") {
-      console.error("Database has too many connections.");
-    }
-    if (err.code === "ECONNREFUSED") {
-      console.error("Dabatase connection was refused.");
-    }
-    if (connection) {
-      connection.release();
-    }
-    return;
-  }
-});
-// promisify all queries to enable async/await
-pool.query = util.promisify(pool.query);
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -69,4 +41,4 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("listening on ", port));
 
-module.exports = pool;
+
