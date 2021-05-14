9390a045ab310388ba9e006fc096d6f2603169d0
Jose Valadez <vinmajj@gmail.com>
2021-05-14 16:45:52 -0700

    create report function create report and get reports

--- server/controllers/report.js
+++ server/controllers/report.js
@@ -1,8 +1,16 @@
-const pool = require('../db/db');
-
+const { createReport , getReports} = require("../model/report");
 
 exports.create_report = async (req, res) => {
-
+    
+    // insert report body
+    await createReport(req.body);
+    try {
+        res.status(200).json('Report created');
+      }catch(err) {
+        res.status(500).json({
+          error: err
+        });
+      }
 };
 
 exports.delete_report = async (req, res) => {
@@ -14,5 +22,12 @@ exports.change_report = async (req, res) => {
 };
 
 exports.get_reports = async (req, res) => {
-    
+    // get all reports
+    try {
+        let response = await getReports();
+        res.status(200).json(response);
+    }catch (err) {
+        res.status(500).json({error: err})
+    }
+
 };
