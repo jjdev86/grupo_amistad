2cd50873554690b3fb46b2aaa681903d8bc97c9d
Jose Valadez <vinmajj@gmail.com>
2021-05-14 16:44:42 -0700

    create report db model

--- server/model/report.js
+++ server/model/report.js
@@ -1,6 +1,29 @@
-const pool = require('../db/db');
+const { pool } = require("../db/db");
 
+const createReport = async (report) => {
+  const sql = 
+        `INSERT INTO report SET ?`;
+  try {
+    console.log(report);
+    let reportCreated = await pool.query(sql, [report]);
+    return reportCreated;
+  } catch (err) {
+    console.log(err, "error?");
+    return err;
+  }
+};
 
-const createReport = async report => {
-    
+const getReports = async () => {
+  const sql = `Select * from report`;
+  try {
+    let report = await pool.query(sql);
+    return report;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = {
+  createReport,
+  getReports,
 };
