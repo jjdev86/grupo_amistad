e69da239f7aa6361e9f24ca4979a4299785f6169
Jose Valadez <vinmajj@gmail.com>
2021-05-14 17:27:52 -0700

    add member route

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const userRoutes = require("./routes/user");
 const reportRoutes = require("./routes/report");
 const grouptRoutes = require('./routes/group');
-
+const memberRoutes = require('./routes/member');
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname + "/../client/dist")));
 app.use("/user", userRoutes);
 app.use('/report', reportRoutes);
 app.use('/group', grouptRoutes);
+app.use('/member', memberRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Route Not found");
@@ -38,6 +39,7 @@ app.use((error, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => console.log("listening on ", port));
 
